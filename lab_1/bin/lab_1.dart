96899548c23566af1b082725710232d4cf74a5f7
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-24 09:41:46 +0300

    Feat:More lists functions

--- lab_1/bin/lab_1.dart
+++ lab_1/bin/lab_1.dart
@@ -12,14 +12,39 @@ void main() {
   name = "Baraka Mbugua";
   print("My name is $name");
   //Null variable
-  dynamic? adm_no;
-  print("My admission number is $adm_no");
+  dynamic admNo;
+  print("My admission number is $admNo");
   //using collections
   var ages = [23, 24, 25, 26, 27];
-  ages.forEach((element) { print(element); }); //Using a for each loop to print the elements
+  ages.forEach((e) {
+    print(e);
+  }); //Using a for each loop to print the elements
   //other for form of for statement
-  for(int age in ages){
+  for (int age in ages) {
     print(age);
   }
-
+  //To compute sum of ages(sum is compunded because its in a for loop)
+  int sum = 0;
+  for (int age in ages) {
+    sum += age;
+    print("The sum of the ages is $sum");
+  }
+  //To compute the average of the ages
+  double average = sum / ages.length;
+  print("The average of the ages is $average");
+  //To add an element to the list
+  ages.add(45);
+  //to delete an element from the list
+  ages.remove(45);
+  //to add multiple elements to the list
+  ages.addAll([45, 46, 47, 48, 49]);
+  //to sort the list
+  ages.sort();
+  //to sort the list in descending order
+  ages.sort((a, b) => b.compareTo(a));
+  //to reverse the list
+  ages.reversed;
+  print(ages);
+  //to print the list
+  print(ages);
 }
