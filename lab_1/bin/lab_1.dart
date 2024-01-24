2487c4368461836e2befa61199fd808cb17f3253
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-24 09:18:13 +0300

    Feat:Used late variable

--- lab_1/bin/lab_1.dart
+++ lab_1/bin/lab_1.dart
@@ -2,11 +2,14 @@ void main() {
   //Dynamic variable to allow age to change from int to string
   dynamic age = 23;
   //To declare a late variable
-  late String name;
+  late dynamic name;
   //print age using string interpolation
   print("I am $age years old");
   //change age to string
   age = "Twenty three";
   print("I am $age years old");
+  //print name
+  name = "Baraka Mbugua";
+  print("My name is $name");
 
 }
