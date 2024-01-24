bd59a4c80d768c021398d24f41c1570699ebca9e
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-24 09:15:57 +0300

    Feat:Learnt string interpolation

--- lab_1/bin/lab_1.dart
+++ lab_1/bin/lab_1.dart
@@ -1,7 +1,12 @@
 void main() {
-  var age = 23;
+  //Dynamic variable to allow age to change from int to string
+  dynamic age = 23;
   //To declare a late variable
   late String name;
-  //print age
-  print(age);
+  //print age using string interpolation
+  print("I am $age years old");
+  //change age to string
+  age = "Twenty three";
+  print("I am $age years old");
+
 }
