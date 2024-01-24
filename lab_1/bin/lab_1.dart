768b06377db467beefb34ecc80d0d9aaaa6ff11a
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-24 09:11:43 +0300

    Feat:Learnt variable declaration

---
+++ lab_1/bin/lab_1.dart
@@ -0,0 +1,7 @@
+void main() {
+  var age = 23;
+  //To declare a late variable
+  late String name;
+  //print age
+  print(age);
+}
---
+++ lab_1/test/lab_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:lab_1/lab_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
