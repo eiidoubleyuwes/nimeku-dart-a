dde1aaf98ed143ea622ce718286c86f377ce44d4
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-31 09:24:20 +0300

    Feat:Calling the functions in the main function

--- lab_methods/bin/lab_methods.dart
+++ lab_methods/bin/lab_methods.dart
@@ -3,6 +3,8 @@ import 'package:lab_methods/lab_methods.dart' as lab_methods;
 void main() {
   print(add(15, 10));
   print(multiply(45, 67));
+  print(divide(9, 9));
+  print(subtract(45 , 8));
 }
 
 add(a, b) => a + b;
