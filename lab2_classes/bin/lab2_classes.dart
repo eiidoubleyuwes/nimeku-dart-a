3a0a07a96afbd0931da1c2adea95429595efbdc2
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-31 09:36:12 +0300

    Feat:Added first class and constructor

--- lab2_classes/bin/lab2_classes.dart
+++ lab2_classes/bin/lab2_classes.dart
@@ -1,4 +1,15 @@
 import 'package:lab2_classes/lab2_classes.dart' as lab2_classes;
 
 void main() {
+  Example ex = Example(1, 2);
+  print(ex.a);
+}
+
+class Example {
+  late int a;
+  late int b;
+  Example(int a, int b) {
+    this.a = a;
+    this.b = b;
+  }
 }
