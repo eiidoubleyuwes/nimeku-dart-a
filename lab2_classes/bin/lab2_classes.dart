6dc8936dd0dae66e89b52f313153a1e21e605fa2
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-31 09:33:40 +0300

    Feat:Learning classes

---
+++ lab2_classes/bin/lab2_classes.dart
@@ -0,0 +1,4 @@
+import 'package:lab2_classes/lab2_classes.dart' as lab2_classes;
+
+void main() {
+}
---
+++ lab2_classes/test/lab2_classes_test.dart
@@ -0,0 +1,8 @@
+import 'package:lab2_classes/lab2_classes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
