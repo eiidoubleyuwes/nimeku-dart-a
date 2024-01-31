27ab50c405570d025aa952a4dbd537c0293c5044
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-31 09:58:25 +0300

    Feat:Fixed deletion

---
+++ lab_flutter/lib/configs/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+//To define the color pallette of the app
+const Color primaryColor = Color(0xFF0D47A1);
+const Color appWhiteColor = Color(0xFFFFFFFF);
+const Color headercolor = Color(0xFF000000);
+const Color textcolorgrey = Color(0xFF757575);
+const Color textcolorlightgrey = Color(0xFFBDBDBD);
+const Color textcolorwhite = Color(0xFFFFFFFF);
+const Color textcolorblack = Color(0xFF000000);
---
+++ lab_flutter/lib/main.dart
@@ -0,0 +1,51 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:lab_flutter/configs/constants.dart';
+import 'package:lab_flutter/views/customtexts.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text('App ya mine'),
+      centerTitle: true,
+      backgroundColor: primaryColor,
+      foregroundColor: appWhiteColor,
+      titleTextStyle: TextStyle(
+          color: headercolor,
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+      )
+    ),
+    body: Column(
+      children: [
+        //Created a widget for the text
+        customText("Username", label: 'Username',),
+        customText("Password", label: 'Password',),
+        customText("Forgot password", label: 'Forgot password?')
+      ],
+    ),
+    );
+  }
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Username', style: TextStyle(
+      color: textcolorgrey,
+      fontSize: 18.0,
+      fontWeight: FontWeight.bold,
+    ));
+  }
+
