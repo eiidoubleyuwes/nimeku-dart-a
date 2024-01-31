8572d47d8a2b3992aca5ba485bf0a8b18ba64a66
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-31 09:20:24 +0300

    Feat:Added second way of methods

--- lab_methods/bin/lab_methods.dart
+++ lab_methods/bin/lab_methods.dart
@@ -2,6 +2,9 @@ import 'package:lab_methods/lab_methods.dart' as lab_methods;
 
 void main() {
   print(add(15, 10));
+  print(multiply(45, 67));
 }
 
 add(a, b) => a + b;
+int multiply(int a, int b) => a * b;
+
