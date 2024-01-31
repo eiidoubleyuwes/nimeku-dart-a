729a84aac2d8b5544926556595cddedcbeebd53e
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-31 09:16:38 +0300

    Feat:Learning Methods in Dart

---
+++ lab_methods/bin/lab_methods.dart
@@ -0,0 +1,5 @@
+import 'package:lab_methods/lab_methods.dart' as lab_methods;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lab_methods.calculate()}!');
+}
---
+++ lab_methods/test/lab_methods_test.dart
@@ -0,0 +1,8 @@
+import 'package:lab_methods/lab_methods.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
