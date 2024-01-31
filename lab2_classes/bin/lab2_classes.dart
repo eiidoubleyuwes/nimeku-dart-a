b86c50b6b3e781468e986e93398cbf99f809a2cc
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-01-31 09:37:13 +0300

    Feat:Added second class and constructor example

--- lab2_classes/bin/lab2_classes.dart
+++ lab2_classes/bin/lab2_classes.dart
@@ -8,8 +8,15 @@ void main() {
 class Example {
   late int a;
   late int b;
+  //First constructor example
   Example(int a, int b) {
     this.a = a;
     this.b = b;
   }
+}
+class Example2 {
+  late int a;
+  late int b;
+  //Second constructor example
+  Example2(this.a, this.b);
 }
