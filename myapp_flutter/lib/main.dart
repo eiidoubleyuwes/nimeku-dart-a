ec5ff9a3ba1c657d045fe9439da4bc70f5b0b273
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-14 08:38:07 +0300

    Feat:More code cleanup
    Deleted unnecessary imports from the main Dart file

--- myapp_flutter/lib/main.dart
+++ myapp_flutter/lib/main.dart
@@ -2,10 +2,6 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:myapp_flutter/configs/constants.dart';
 import 'package:myapp_flutter/pages/login.dart';
-import 'package:myapp_flutter/pages/registration.dart';
-import 'package:myapp_flutter/views/custombutton.dart';
-import 'package:myapp_flutter/views/customcontroller.dart';
-import 'package:myapp_flutter/views/customtexts.dart';
 
 void main() {
   runApp(MaterialApp(
