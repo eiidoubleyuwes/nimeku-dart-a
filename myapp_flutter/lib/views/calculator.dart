5ec30227e9f7a0a08b2e6cb9393941b4ddf024ea
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-14 08:52:00 +0300

    Feat:Added code
    Learning stateful widgets

---
+++ myapp_flutter/lib/views/calculator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
