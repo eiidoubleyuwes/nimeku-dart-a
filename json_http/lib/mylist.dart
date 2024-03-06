a234d146e1bedd90300760aea0741a70777e9afa
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-06 09:45:09 +0300

    Feat:Added necessary models
    This is cool because I can now deal with importing and dealing with the raw Json File

---
+++ json_http/lib/mylist.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MyListView extends StatelessWidget {
+  const MyListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
