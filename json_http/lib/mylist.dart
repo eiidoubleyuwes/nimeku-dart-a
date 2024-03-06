a6a65563958d143a7f1c77be0c17fe6b015420e1
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-06 09:55:49 +0300

    DEFEAT:BAD CODE
    I have defined methods and classes wrongly,will fix later

--- json_http/lib/mylist.dart
+++ json_http/lib/mylist.dart
@@ -6,6 +6,34 @@ class MyListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: FutureBuilder(
+        future: fetchAlbum(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data[index].title),
+                  subtitle: Text(snapshot.data[index].id.toString()),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+  ListfetchAlbum() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+    if (response.statusCode == 200) {
+      return AlbumFromJson(response.body);
+    } else {
+      throw Exception('Failed to load album');
+    }
   }
 }
